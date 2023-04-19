5d766fc897d347035341da53070e787fd9beaede
dj-nuo <djnuo95@gmail.com>
2023-04-19 15:49:31 +0200

    prettier svelte

--- src/main/lib/utils.ts
+++ src/main/lib/utils.ts
@@ -1,23 +1,23 @@
-import { app, ipcMain, BrowserWindow } from 'electron'
-import os from 'os'
+import { app, ipcMain, BrowserWindow } from 'electron';
+import os from 'os';
 
 ipcMain.on('appVersionSync', (event, arg) => {
-  console.log(arg)
-  let currentVersion = ''
+	console.log(arg);
+	let currentVersion = '';
 
-  if (process.env.NODE_ENV === 'development') {
-    currentVersion = process.env.npm_package_version || ''
-  } else {
-    currentVersion = app.getVersion()
-  }
-  event.returnValue = currentVersion
-})
+	if (process.env.NODE_ENV === 'development') {
+		currentVersion = process.env.npm_package_version || '';
+	} else {
+		currentVersion = app.getVersion();
+	}
+	event.returnValue = currentVersion;
+});
 
 ipcMain.on('homeDirPathAsync', (event, arg) => {
-  console.log(event.isTrusted)
-  console.log(arg)
-  const response = os.homedir()
+	console.log(event.isTrusted);
+	console.log(arg);
+	const response = os.homedir();
 
-  const mainWindow = BrowserWindow.getAllWindows()[0]
-  mainWindow.webContents.send('homeDirPathAsyncResponse', response)
-})
+	const mainWindow = BrowserWindow.getAllWindows()[0];
+	mainWindow.webContents.send('homeDirPathAsyncResponse', response);
+});
