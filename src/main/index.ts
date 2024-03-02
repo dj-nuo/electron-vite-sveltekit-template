1e6ae412b5035624293bbd2d99e25690a5f11693
tsekiguchi <sekiguchi.tim@gmail.com>
2024-03-02 10:58:35 -0800

    Update dependencies

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,106 +1,106 @@
-import { app, shell, BrowserWindow } from 'electron'
-import { join } from 'path'
-import { electronApp, optimizer, is } from '@electron-toolkit/utils'
-import icon from '../../resources/icon.png?asset'
-import serve from 'electron-serve'
-import windowStateManager from 'electron-window-state'
+import { app, shell, BrowserWindow } from 'electron';
+import { join } from 'path';
+import { electronApp, optimizer, is } from '@electron-toolkit/utils';
+import icon from '../../resources/icon.png?asset';
+import serve from 'electron-serve';
+import windowStateManager from 'electron-window-state';
 
-import './lib/utils'
+import './lib/utils';
 
-const serveURL = serve({ directory: join(__dirname, '..', 'renderer') })
+const serveURL = serve({ directory: join(__dirname, '..', 'renderer') });
 
-let mainWindow: BrowserWindow
+let mainWindow: BrowserWindow;
 function createWindow(): void {
-  // window state is useful when application is closed & opened again
-  // especially useful in dev, when app is reloaded on main/preload file changes
-  const windowState = windowStateManager({
-    defaultWidth: 800,
-    defaultHeight: 600
-  })
+	// window state is useful when application is closed & opened again
+	// especially useful in dev, when app is reloaded on main/preload file changes
+	const windowState = windowStateManager({
+		defaultWidth: 800,
+		defaultHeight: 600
+	});
 
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: windowState.width,
-    height: windowState.height,
-    x: windowState.x,
-    y: windowState.y,
-    show: false,
-    autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
-    webPreferences: {
-      preload: join(__dirname, '../preload/index.js'),
-      sandbox: false
-    }
-  })
+	// Create the browser window.
+	mainWindow = new BrowserWindow({
+		width: windowState.width,
+		height: windowState.height,
+		x: windowState.x,
+		y: windowState.y,
+		show: false,
+		autoHideMenuBar: true,
+		...(process.platform === 'linux' ? { icon } : {}),
+		webPreferences: {
+			preload: join(__dirname, '../preload/index.js'),
+			sandbox: false
+		}
+	});
 
-  // initialize window state
-  windowState.manage(mainWindow)
-  // and save state when exiting
-  mainWindow.on('close', () => {
-    windowState.saveState(mainWindow)
-  })
+	// initialize window state
+	windowState.manage(mainWindow);
+	// and save state when exiting
+	mainWindow.on('close', () => {
+		windowState.saveState(mainWindow);
+	});
 
-  mainWindow.on('ready-to-show', () => {
-    mainWindow.show()
-  })
+	mainWindow.on('ready-to-show', () => {
+		mainWindow.show();
+	});
 
-  mainWindow.webContents.setWindowOpenHandler((details) => {
-    shell.openExternal(details.url)
-    return { action: 'deny' }
-  })
-  // HMR for renderer base on electron-vite cli.
-  // Load the remote URL for development or the local html file for production.
-  if (is.dev) {
-    loadVite()
-    mainWindow.webContents.openDevTools()
-    // mainWindow.loadURL(import.meta.env.MAIN_VITE_ELECTRON_RENDERER_URL)
-  } else {
-    // mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
-    serveURL(mainWindow)
-  }
+	mainWindow.webContents.setWindowOpenHandler((details) => {
+		shell.openExternal(details.url);
+		return { action: 'deny' };
+	});
+	// HMR for renderer base on electron-vite cli.
+	// Load the remote URL for development or the local html file for production.
+	if (is.dev) {
+		loadVite();
+		mainWindow.webContents.openDevTools();
+		// mainWindow.loadURL(import.meta.env.MAIN_VITE_ELECTRON_RENDERER_URL)
+	} else {
+		// mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+		serveURL(mainWindow);
+	}
 }
 
 // this is needed to prevent blank screen when dev electron loads
 function loadVite(): void {
-  mainWindow.loadURL(import.meta.env.MAIN_VITE_ELECTRON_RENDERER_URL).catch((e) => {
-    console.log('Error loading URL, retrying', e)
-    setTimeout(() => {
-      loadVite()
-    }, 200)
-  })
+	mainWindow.loadURL(import.meta.env.MAIN_VITE_ELECTRON_RENDERER_URL).catch((e) => {
+		console.log('Error loading URL, retrying', e);
+		setTimeout(() => {
+			loadVite();
+		}, 200);
+	});
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
+	// Set app user model id for windows
+	electronApp.setAppUserModelId('com.electron');
 
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-  app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window)
-  })
+	// Default open or close DevTools by F12 in development
+	// and ignore CommandOrControl + R in production.
+	// see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+	app.on('browser-window-created', (_, window) => {
+		optimizer.watchWindowShortcuts(window);
+	});
 
-  createWindow()
+	createWindow();
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+	app.on('activate', function () {
+		// On macOS it's common to re-create a window in the app when the
+		// dock icon is clicked and there are no other windows open.
+		if (BrowserWindow.getAllWindows().length === 0) createWindow();
+	});
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
